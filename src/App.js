aa6d6c39a67daab206ac01f89d55c5b7d4f655c7
DEVANAND GIRI <g.dev9716@gmail.com>
2022-08-28 18:16:33 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card';
+import Sdata from './Components/Sdata';
+
+const ncard= (currE)=>{
+  return(
+    <Card 
+    imgSrc={currE.imagescr} 
+    title={currE.title}  
+    sname={currE.sname} 
+    link={currE.links}/>
+  );
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <>
+    <h1 className='heading'>List of Top 10 Netflix Series in 2022</h1>
+
+    {Sdata.map(ncard)}
+       
+    </>
   );
 }
 
